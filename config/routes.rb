69e2bfc1ab501542e89c0156ca4b7806439fd4c1
Tomasz Hanusiak <tomhanusiak@gmail.com>
2015-05-27 18:01:05 +0000

    blog completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'articles#index'
-  resources :articles # symbol szybszy niz String
+  resources :articles do
+    resources :comments
+  end
 end
